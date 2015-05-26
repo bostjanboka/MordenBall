2bb2c9a495e4aa52cffb752cd319cad21e0e45b2
bostjanboka <bohtebostjan@gmail.com>
2015-05-26 02:06:14 +0200

    zoga

---
+++ MordenBall/Assets/InputNavigacija.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputNavigacija : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject zoga;
+	public Canvas canvas;
+
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void PlayGumb(){
+		Instantiate (zoga);
+		canvas.enabled = false;
+
+	}
+}
---
+++ MordenBall/Assets/ZogaSkripta.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZogaSkripta : MonoBehaviour {
+
+	// Use this for initialization
+	Rigidbody2D rigid;
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody2D> ();
+		rigid.velocity = new Vector2 (10,-10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
